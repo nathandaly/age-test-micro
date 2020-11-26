a1fa91a8ea32ae897d72e8b91554fab492626f1b
Nathan Daly <nathandalypretoria@hotmail.co.uk>
2020-11-26 10:13:54 +0000

    Wiring up eloquent ORM to communicate with the database in order to store name and age data to meet one of the acceptance criteria.

--- core/settings.php
+++ core/settings.php
@@ -10,10 +10,21 @@ use Dotenv\Dotenv;
 return static function (ContainerBuilder $containerBuilder) {
     $settingsArray = Dotenv::createArrayBacked(__DIR__ . '/../')->load();
 
-    $settingsArray['displayErrorDetails'] = true;
+    $settings['displayErrorDetails'] = $settingsArray['DISPLAY_ERRORS'] ?? false;
+
+    $settings['db'] = [
+        'driver' => 'mysql',
+        'host' => $settingsArray['DB_HOST'] ?? 'localhost',
+        'database' => $settingsArray['DB_NAME'] ?? 'database',
+        'username' => $settingsArray['DB_USER'] ?? 'root',
+        'password' => $settingsArray['DB_PASS'] ?? 'password',
+        'charset'   => $settingsArray['DB_CHARSET'] ?? 'utf8',
+        'collation' => $settingsArray['DB_COLLATION'] ?? 'utf8_unicode_ci',
+        'prefix'    => $settingsArray['DB_PREFIX'] ?? '',
+    ];
 
     // Global Settings Object
     $containerBuilder->addDefinitions([
-        'settings' => $settingsArray,
+        'settings' => $settings,
     ]);
 };
